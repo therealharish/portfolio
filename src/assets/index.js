314dbfad9c9f839fd786358a9eb63ff36c24179f
Harish <48938639+therealharish@users.noreply.github.com>
2023-04-01 20:47:34 +0530

    commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import thebastionclub from "./company/thebastionclub.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -55,6 +56,7 @@ export {
   meta,
   shopify,
   starbucks,
+  thebastionclub,
   tesla,
   carrent,
   jobit,
