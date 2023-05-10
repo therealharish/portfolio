f4337f73b34d8b33a55c8a81f1d36f5a17a8b502
Harish <48938639+therealharish@users.noreply.github.com>
2023-05-10 23:39:56 +0530

    update

--- Python Problems/Day12 Ques1.py
+++
@@ -1,5 +0,0 @@
-def domain(email):
-    return email.split('@')[1].split('.com')[0]
-
-print(domain('amit@sccilabs.com'))
-print(domain('simran@iitpr.com'))
--- Python Problems/Regex.py
+++
@@ -1,8 +0,0 @@
-import re
-pattern = '^a...f$'
-string = 'abcdf'
-result = re.match(pattern, string)
-if result:
-    print("Match")
-else:
-    print("No match")
